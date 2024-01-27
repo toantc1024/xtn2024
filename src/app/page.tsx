94f36257aa01ae3ec2f5a2882489938180a59d9a
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-01-27 13:20:37 +0700

    Update: Restructure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import Intro from "../components/Intro";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Info from "../components/Info";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="h-full w-full">
       <Navbar />
+      <Intro />
       <Hero />
       <Info />
       <Gallery />
--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
-const index = () => {
+import SectionWrapper from '../SectionWrapper'
+const Intro = () => {
   return (
-    <div>index</div>
+    <SectionWrapper background={"bg-white"}>
+        <div className='w-full h-full flex items-center justify-between px-96'>
+            <div className='text-6xl'>
+                <h2>XÃ KIỂNG PHƯỚC</h2>
+                <span>Vùng đất anh hùng</span>
+            </div>
+
+            <div className='w-[500px] h-[500px] rounded-full bg-red-500'>
+                
+            </div>
+
+        </div>
+    </SectionWrapper>
   )
 }
 
-export default index
+export default Intro
