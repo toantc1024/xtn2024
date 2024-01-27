01468476bc30aa5cf12645dc44b3d250be0195d0
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-01-27 15:37:16 +0700

    Update: Intro + image

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import SectionWrapper from '../SectionWrapper'
+import React from "react";
+import SectionWrapper from "../SectionWrapper";
 const Intro = () => {
   return (
     <SectionWrapper background={"bg-white"}>
-        <div className='w-full h-full flex items-center justify-between px-96'>
-            <div className='text-6xl'>
-                <h2>XÃ KIỂNG PHƯỚC</h2>
-                <span>Vùng đất anh hùng</span>
-            </div>
+      <div className="w-full h-full flex items-center justify-between px-96">
+        <div className="bg-gradient-to-b from-red-600 via-yellow-500 to-yellow-400 inline-block text-transparent bg-clip-text flex flex-col text-6xl py-8 gap-4">
+          <div>
+            <h2 className="font-bold">XÃ KIỂNG PHƯỚC</h2>
+            <span className=" font-bold">Vùng đất anh hùng</span>
+          </div>
 
-            <div className='w-[500px] h-[500px] rounded-full bg-red-500'>
-                
-            </div>
-
+          <div>
+            <p className="text-2xl  m-2">
+              Quê tôi ở tận miền tây
+              <br />
+              Mênh mông đồng lúa cò bay đầy trời
+              <br />
+              Đất lành chim đậu khắp nơi
+              <br />
+              Mời anh mời chị đến chơi quê nhà.
+            </p>
+          </div>
         </div>
-    </SectionWrapper>
-  )
-}
 
-export default Intro
+        <div className="w-[500px] h-[500px] rounded-full bg-red-500"></div>
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default Intro;
