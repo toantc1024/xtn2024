df25c4fb9128bfb29e669edb34f70d6e502bf64e
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-01-29 13:26:34 +0700

    update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
+import { FaArrowLeft } from "react-icons/fa6";
+import { HiArrowCircleLeft, HiMenu } from "react-icons/hi";
 
 // Script to scroll to an element (by Id) and smooth for me
 const scrollToSection = (id) => {
@@ -13,40 +15,6 @@ const Navbar = () => {
   const aboutRef = useRef(null);
   const servicesRef = useRef(null);
   // add more refs for other sections
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          const id = entry.target.getAttribute("id");
-          if (entry.isIntersecting) {
-            document
-              .querySelector(`.nav__menu a[href="#${id}"]`)
-              .classList.add("active");
-          } else {
-            document
-              .querySelector(`.nav__menu a[href="#${id}"]`)
-              .classList.remove("active");
-          }
-        });
-      },
-      { threshold: 0.7 } // adjust this value for when to trigger the change
-    );
-
-    if (aboutRef.current) {
-      observer.observe(aboutRef.current);
-    }
-
-    if (servicesRef.current) {
-      observer.observe(servicesRef.current);
-    }
-
-    // observe other sections
-
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
 
   const hrefLinks = [
     {
@@ -70,6 +38,7 @@ const Navbar = () => {
       label: "Liên hệ",
     },
   ];
+  const [currentLabel, setCurrentLabel] = useState(hrefLinks[0].label);
 
   useEffect(() => {
     function navbarlinksActive() {
@@ -111,6 +80,41 @@ const Navbar = () => {
     }
   }
 
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const id = entry.target.getAttribute("id");
+          if (entry.isIntersecting) {
+            document
+              .querySelector(`.nav__menu a[href="#${id}"]`)
+              .classList.add("active");
+          } else {
+            document
+              .querySelector(`.nav__menu a[href="#${id}"]`)
+              .classList.remove("active");
+          }
+        });
+      },
+      { threshold: 0.7 } // adjust this value for when to trigger the change
+    );
+
+    if (aboutRef.current) {
+      observer.observe(aboutRef.current);
+    }
+
+    if (servicesRef.current) {
+      observer.observe(servicesRef.current);
+    }
+
+    // observe other sections
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
   return (
     <div
       className={`bg-orange-500 p-2  md:py-4 md:px-8 flex justify-between w-full  fixed z-[9999] shadow-sm nav__menu`}
@@ -126,6 +130,53 @@ const Navbar = () => {
               <a
                 className="nav_link"
                 onClick={() => view(link.href)}
+                href={link.href}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="text-sm md:text-2xl text-white flex xl:hidden gap-2 justify-between items-center">
+        <div className="relative">
+          <button
+            onClick={() => {
+              setShowDropDown(!showDropDown);
+            }}
+            id="dropdownDividerButton"
+            data-dropdown-toggle="dropdownDivider"
+            class="p-2 rounded-full bg-white-400  text-4xl"
+            type="button"
+          >
+            <HiMenu />
+          </button>
+        </div>
+      </div>
+      <div
+        className={`${
+          showDropDown ? "fixed" : "hidden"
+        } left-0 flex flex-col justify-between items-center top-0 z-[9999] bg-[rgba(255,255,255,.9)] w-full h-screen`}
+      >
+        <div className="py-8 flex items-center justify-start w-full px-6 text-4xl ">
+          <button
+            onClick={() => {
+              setShowDropDown(!showDropDown);
+            }}
+            className="p-2 rounded-full text-orange-600 border-[1px] border-orange-500 shadow-orange-200 shadow-lg bg-white"
+          >
+            <FaArrowLeft />
+          </button>
+        </div>
+        <ul className="bg-[rgba(255,255,255,.4)] w-full h-full flex flex-col gap-4 justify-center text-4xl items-center ">
+          {hrefLinks.map((link) => (
+            <li key={link.href}>
+              <a
+                className="nav_link"
+                onClick={() => {
+                  view(link.href);
+                  setShowDropDown(!showDropDown);
+                }}
                 href={link.href}
               >
                 {link.label}
